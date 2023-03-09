6ff0615fb57cbb5e27ff3c628aa7d4f9f4bac109
Ulices Salvador Aguila Contreras <Ulices13SAC@outlook.es>
2023-03-08 23:02:42 -0600

    second commit

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -6,7 +6,7 @@ const LocationInfo = ({location}) => {
     <section className='locationInfo'>
         <h2 className='locationInfo__name'>{location?.name}</h2>
         <ul className='locationInfo__list'>
-          <li className='locationInfo__item'><span>Type:</span> {location?.type}</li>
+          <li className='locationInfo__item'><span>Type:</span>{location?.type}</li>
           <li className='locationInfo__item'><span>Dimension:</span> {location?.dimension} </li>
           <li className='locationInfo__item'><span>Population:</span> {location?.residents.length} </li>
         </ul>
